f28bb8ec15c7757ed993ba10ae5b1d4425259a39
CoffeeKiller <18320210361@163.com>
2023-12-19 22:54:39 +0800

    feat: add jwtFunc and IPAddressFunc

--- server/.eslintrc.cjs
+++ server/.eslintrc.cjs
@@ -1,11 +1,12 @@
 /* eslint-env node */
 module.exports = {
     root: true,
-
+    'parser': '@typescript-eslint/parser',
     extends: ["eslint:recommended"],
     parserOptions: {
         ecmaVersion: "latest",
-        sourceType: "module"
+        sourceType: "module",
+        'parser': 'babel-eslint',
     },
     env: {
         "node": true,
--- server/utils/util.ts
+++ server/utils/util.ts
@@ -1,5 +1,8 @@
-// import { Context } from 'vm'
-// import jwt from "jsonwebtoken"
+import { Context } from 'vm'
+import jwt, { Jwt } from 'jsonwebtoken'
+import { JWT } from '../config/constant'
+
+
 
 /**
  * @description: 获取当前服务的ip地址
@@ -16,4 +19,42 @@ export const getIPAddress = () => {
       }
     }
   }
+}
+
+/**
+ * @description: 获取客户端ip地址
+ * @param ctx {Context} : 上下文对象
+ * @return ipAddr {string} : ip地址字符串
+ * */
+export const getClientIPAddress = (ctx: Context):string => {
+  const  headers = ctx.headers
+
+  if (headers["x-forwarded-for"]) {
+    const ipList:string = headers["x-forwarded-for"].split(",")
+    return ipList
+  }
+  return "0.0.0.0"
+}
+
+/**
+ * @description: 解析token获取userid
+ * @param token {string} : token
+ * @return number/string
+ * */
+export const decodeToken = (token: string) => {
+  let jwtInfo = jwt.verify(token, JWT.secret) as any
+  try {
+    return jwtInfo.userId as number
+  } catch (err) {
+    return  "token不合法"
+  }
+}
+
+/**
+ * @description: 根据userId生成token
+ * @param userId {number} : userid
+ * @return token
+ * */
+export const generatorToken = (userId: number): string => {
+  return jwt.sign( {userId}, JWT.secret, { expiresIn: JWT.expires })
 }
