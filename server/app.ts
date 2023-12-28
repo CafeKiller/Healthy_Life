9830e7e80aeac36d566e32499ab491d8522f137e
CoffeeKiller <18320210361@163.com>
2023-12-28 22:17:41 +0800

    feat: 调整中间件

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,23 @@
 import http from "http"
 import Koa from "koa"
+import { loggerMiddleware } from './log/log'
+import koaBody from 'koa-body'
+import { errorHandler } from './middleware/response'
+import { FIXED_KEY } from './config/constant'
 
 // 创建 Koa 容器对象
 const app = new Koa()
 
-// 获取端口号
-const port = 8888
+// 使用日志中间件
+app.use(loggerMiddleware)
+// 使用异常处理中间件
+app.use(errorHandler)
+// 开启body解析
+app.use(koaBody({multipart: true}))
 
+
+// 获取端口号
+const port = FIXED_KEY.port
 // 创建服务
 const server = http.createServer(app.callback)
 // 服务监听端口
--- server/middleware/response.ts
+++ server/middleware/response.ts
@@ -1,8 +1,10 @@
 import Koa from 'koa'
 import { CODE } from '../config/code'
+import { logger } from '../log/log'
 
+// 该中间件主要用于ctx.result的内容今夕最终封装, 并返回给客户端
 export const responseHandler = (ctx: Koa.Context) => {
-  if (ctx.body !== undefined) {
+  if (ctx.request.body !== undefined) {
     ctx.type = "json"
     ctx.body = {
       code: CODE.success.code,
@@ -10,4 +12,28 @@ export const responseHandler = (ctx: Koa.Context) => {
       message: CODE.success.message
     }
   }
+}
+
+// 该中间件主要用用于处理其他中间件出现的异常, 在next()后进行异常捕获, 出现异常后直接由该中间件进行处理
+export const errorHandler = (ctx: Koa.Context, next: Koa.Next) => {
+  return next().catch((err) => {
+    if (typeof err === "object") {
+      ctx.body = {
+        code: err.code,
+        data: null,
+        message: err.message
+      }
+    } else {
+      ctx.body = {
+        code: -1,
+        data: null,
+        message: err
+      }
+    }
+
+    logger.error(err)
+    // 保证返回值200
+    ctx.status = 200
+    return Promise.resolve()
+  })
 }
