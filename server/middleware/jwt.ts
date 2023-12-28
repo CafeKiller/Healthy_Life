451649df29572f2f66630ba194d7eb036b108bb2
CoffeeKiller <18320210361@163.com>
2023-12-28 21:23:52 +0800

    feat: 包装JWT token

---
+++ server/middleware/jwt.ts
@@ -0,0 +1,18 @@
+import Koa from 'koa'
+import { decodeToken } from '../utils/util'
+import { CODE } from '../config/code'
+
+export const jwtMiddleware = async (ctx: Koa.Context, next:Koa.Next) => {
+  const token = ctx.request.headers.token
+  if (typeof token === "string") {
+    try {
+      let userId = decodeToken(token)
+      console.log(userId)
+    } catch (err) {
+      throw CODE.tokenFailed
+    }
+  } else {
+    throw CODE.tokenFailed
+  }
+  return next()
+}
