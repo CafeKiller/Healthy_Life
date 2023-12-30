2a48e97dea3593316f207e48b5bf236fd5668e42
CoffeeKiller <18320210361@163.com>
2023-12-31 00:32:31 +0800

    feat: update jwt verify logic

--- server/middleware/jwt.ts
+++ server/middleware/jwt.ts
@@ -1,13 +1,22 @@
 import Koa from 'koa'
 import { decodeToken } from '../utils/util'
 import { CODE } from '../config/code'
+import { getUserInfosService } from '../services/user/user'
 
 export const jwtMiddlewareDeal = async (ctx: Koa.Context, next:Koa.Next) => {
   const token = ctx.request.headers.token
   if (typeof token === "string") {
     try {
       let userId = decodeToken(token)
-      console.log(userId)
+      if (typeof userId === 'string' )  throw CODE.tokenFailed
+
+      let userInfo = await getUserInfosService(userId)
+      if (!userInfo) {
+        throw CODE.tokenFailed
+      } else {
+        ctx.userId = userId
+        ctx.userInfo = userInfo
+      }
     } catch (err) {
       throw CODE.tokenFailed
     }
