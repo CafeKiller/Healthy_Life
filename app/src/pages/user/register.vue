cea92e1f38faefa837bb18e7c733db922ebcdf8e
CoffeeKiller <18320210361@163.com>
2024-01-13 21:55:56 +0800

    feat: udpate registerPage

--- app/src/pages/user/register.vue
+++ app/src/pages/user/register.vue
@@ -1,6 +1,14 @@
 <template>
     <view class="content">
         <view class="title">REGISTER</view>
+        <view class="register-info">
+            <input class="uni-input account-input" v-model="user_info.account" type="number"  focus placeholder="用户账号(建议使用手机号)" />
+            <input class="uni-input password-input" v-model="user_info.password" password  placeholder="密码" />
+            <input class="uni-input username-input" v-model="user_info.username" placeholder="用户昵称"></input>
+            <input class="uni-input age-input" v-model="user_info.age" type="number" placeholder="年龄">
+            <input class="uni-input sex-input" v-model="user_info.sex"  placeholder="性别">
+            <input class="uni-input email-input" v-model="user_info.email"  placeholder="用户邮箱">
+        </view>
     </view>
 </template>
 
@@ -9,8 +17,12 @@ export default {
     data() {
         return {
             user_info: {
-                account: ""
-
+                username: "",
+                account: "",
+                password: "",
+                age: null,
+                sex: "",
+                email: "",
             }
         };
     }
