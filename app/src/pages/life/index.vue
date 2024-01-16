8d7109655480804323995464c9062dc2b22b527c
CoffeeKiller <18320210361@163.com>
2024-01-16 22:05:33 +0800

    feat: update listPage

--- app/src/pages/life/index.vue
+++ app/src/pages/life/index.vue
@@ -4,10 +4,18 @@
         <view class="header"></view>
         <!--主体-->
         <view class="wrap">
-            <div class="item"></div>
-            <div class="item"></div>
-            <div class="item"></div>
-            <div class="item"></div>
+            <div class="item">
+                <view class="title">体重变化</view>
+            </div>
+            <div class="item">
+                <view class="title">运动锻炼</view>
+            </div>
+            <div class="item">
+                <view class="title">睡眠质量</view>
+            </div>
+            <div class="item">
+                <view class="title">饮食健康</view>
+            </div>
         </view>
     </view>
 </template>
@@ -29,29 +37,5 @@ export default {
 </script>
 
 <style scoped>
-.header {
-    padding: 20px 10px 35px;
-    width: 100%;
-    height: 365px;
-    border-radius: 0 0 45px 45px;
-    background-color: #fff;
-    box-shadow: 2px 2px 10px rgba(0,0,0,.15);
-}
-
-.wrap {
-    padding: 30px 6px 0;
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: space-around;
-}
-.item {
-    margin-bottom: 20px;
-    width: 185px;
-    height: 240px;
-    border-radius: 10px;
-    background-color: #fff;
-    box-shadow: 2px 2px 10px rgba(0,0,0,.1);
-}
+@import "./style/index.scss";
 </style>
