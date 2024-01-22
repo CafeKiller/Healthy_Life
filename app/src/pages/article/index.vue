5373e1e7264e42af9614a13e79227329e0ae6f98
CoffeeKiller <18320210361@163.com>
2024-01-22 23:01:10 +0800

    feat: add input article keyword detection

--- app/src/pages/article/index.vue
+++ app/src/pages/article/index.vue
@@ -1,8 +1,8 @@
 <template>
-    <view class="content">
+    <view class="content" >
         <view class="search-cont">
-            <input class="search-input" type="input" placeholder="请输入相关文章关键字"></input>
-            <view class="search-btn" size="mini" >
+            <input class="search-input" @input='onInputKey' type="input" placeholder="请输入相关文章关键字"></input>
+            <view class="search-btn" size="mini" @tap.stop='searchArticle'>
                 <span class="iconfont icon-fangdajing"></span>
             </view>
         </view>
@@ -17,7 +17,7 @@
             </div>
         </view>
         <view class="article-list">
-            <view v-for="(item, index) in articleList" :key="index">
+            <view v-for="(item, index) in articleList" :key="item.aid">
                 <view class="article-item">
                     <view class="article-image">
                         <img :src="img_url_prefix + item.image" alt="">
@@ -41,20 +41,18 @@ export default {
         return {
             title: '文章页面',
             img_url_prefix: "http://localhost:9999/project/HL/static/",
-            articleList: null
+            articleList: [],
+            inputKey : "请输入相关文章关键字",
         }
     },
     computed: {
-        articleList(){
-            return
-        }
     },
     onLoad() {
+        // 页面加载时 同时加载新闻列表
         uni.request({
             url: "/api/article/all",
             method: "GET",
             success: (res) => {
-                console.log(res)
                 if(res.data.data) {
                     this.articleList = res.data.data.result
                 }
@@ -62,7 +60,33 @@ export default {
         })
     },
     methods: {
-
+        // HACK 处理 uniApp 数据双向绑定BUG
+        onInputKey(event){
+            this.inputKey = event.detail.value
+        },
+        /**
+         * 搜索文章函数, 需要先检查用户输入的关键字是否合法
+         * */
+        searchArticle(){
+            if (!this.detectionInputKey(String(this.inputKey))) {
+                uni.showModal({
+                    title: "警告",
+                    content: "请输入合法的关键字",
+                    showCancel: false
+                })
+            }
+        },
+        /**
+         * 检查用户输入的文章关键字是否合法
+         * 主要检查 字符是否为空 字符是否为全数字
+         * @param {string} key 关键字
+         * @return {boolean} 返回检查合法性
+         * */
+        detectionInputKey(key){
+            if (key) return false
+            if (/^\d+$/.test(key)) return false
+            return true
+        }
     }
 }
 </script>
