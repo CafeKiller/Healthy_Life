d140b9de8c6b12314a442e2152eac06d4690ac96
CoffeeKiller <18320210361@163.com>
2024-01-25 22:40:34 +0800

    feat: Update DayDataDialog

--- app/src/components/dialog/dialogDayData.vue
+++ app/src/components/dialog/dialogDayData.vue
@@ -2,15 +2,39 @@
   <div class='dialog'>
     <div class='content dialog-day-data'>
       <div class='title'>今日数据更新</div>
-      <div class='data-list'>
-        <div class='data-item header'>
+      <div class='data-cont'>
+        <div class='header'>
           <div class='col left'>当日</div>
           <div class='col right'>规划</div>
         </div>
-        <div class='data-item'></div>
-        <div class='data-item'></div>
-        <div class='data-item'></div>
-        <div class='data-item'></div>
+        <div class='data-list'>
+          <div class='data-item'>
+            <input class='col left' placeholder='今日体重'></input>
+            <div class='col right'></div>
+          </div>
+          <div class='data-item'>
+            <input class='col left' placeholder='今日卡路里摄入'></input>
+            <div class='col right'></div>
+          </div>
+          <div class='data-item'>
+            <input class='col left' placeholder='今日运动步数'></input>
+            <div class='col right'></div>
+          </div>
+          <div class='data-item'>
+            <input class='col left' placeholder='今日有氧运动分钟'></input>
+            <div class='col right'></div>
+          </div>
+          <div class='data-item'>
+            <input class='col left' placeholder='今日睡眠时长'></input>
+            <div class='col right'></div>
+          </div>
+<!--          <div class='data-item'></div>-->
+        </div>
+        <div class='tips-list'>
+          <div class='tips'>* 规划数据只作展示, 不可以在当前页面进行修改</div>
+          <div class='tips color'>* 数据直接输入数值即可, 可以不用输入单位</div>
+          <div class='tips color'>* 食物可以直接输入食物名, 会自动转换为卡路里量</div>
+        </div>
       </div>
       <div class='btn-close iconfont icon-cha' @tap.stop='closeDialog()'></div>
     </div>
