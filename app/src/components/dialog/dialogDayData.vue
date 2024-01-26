a1abc101af05833feee6c6dc5bd9f8790f6f8e90
CoffeeKiller <18320210361@163.com>
2024-01-26 22:59:42 +0800

    feat: update dialogDayData

--- app/src/components/dialog/dialogDayData.vue
+++ app/src/components/dialog/dialogDayData.vue
@@ -9,27 +9,27 @@
         </div>
         <div class='data-list'>
           <div class='data-item'>
-            <input class='col left' placeholder='今日体重'></input>
+            <input class='col left' placeholder='今日体重' ></input>
             <div class='col right'></div>
           </div>
           <div class='data-item'>
-            <input class='col left' placeholder='今日卡路里摄入'></input>
+            <input class='col left' type='input' v-model='day_data.calorie' placeholder='今日卡路里摄入'></input>
             <div class='col right'></div>
           </div>
           <div class='data-item'>
-            <input class='col left' placeholder='今日运动步数'></input>
+            <input class='col left' placeholder='今日运动步数' v-model='day_data.step_num'></input>
             <div class='col right'></div>
           </div>
           <div class='data-item'>
-            <input class='col left' placeholder='今日有氧运动分钟'></input>
+            <input class='col left' placeholder='今日有氧运动分钟' v-model='day_data.exercise_time'></input>
             <div class='col right'></div>
           </div>
           <div class='data-item'>
-            <input class='col left' placeholder='今日睡眠时长'></input>
+            <input class='col left' placeholder='今日睡眠时长' v-model='day_data.sleep_time'></input>
             <div class='col right'></div>
           </div>
-<!--          <div class='data-item'></div>-->
         </div>
+        <div class='update-btn' @tap.stop='updateDayDate'>UPDATE</div>
         <div class='tips-list'>
           <div class='tips'>* 规划数据只作展示, 不可以在当前页面进行修改</div>
           <div class='tips color'>* 数据直接输入数值即可, 可以不用输入单位</div>
@@ -45,6 +45,17 @@
 export default {
   name:"DialogDayData",
   props:["isShowCommonDialog"],
+  data() {
+    return {
+      day_data:{
+        calorie: "",
+        sleep_time: "",
+        step_num: "",
+        exercise_time: "",
+        foods:"",
+      }
+    }
+  },
   created(){
 
   },
@@ -54,6 +65,15 @@ export default {
      * */
     closeDialog(){
       this.$emit("UpdateDialogDayDataState", !this.isShowCommonDialog)
+    },
+    updateDayDate(){
+
+    },
+    /**
+     * 处理需要上传的每日数据格式
+     * */
+    disposeDayData(){
+
     }
   },
 }
